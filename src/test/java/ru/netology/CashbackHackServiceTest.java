780ead919146eadb23b289fcd840cdb39befb1b8
Dmitry Urin <domainlover@yahoo.com>
2021-01-10 22:32:25 +0200

    Files added

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void amountLowerThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void amountHigherThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void amountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
+
+
